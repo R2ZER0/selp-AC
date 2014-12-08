9b396aa7eb6ae8df5b25c285ebc42ad9dada04ec
Rikki Guy <git@rikkiguy.me.uk>
2014-12-08 03:10:59 +0000

    Add header guards to squarestats_server_event.h

--- source/src/squarestats_server_event.h
+++ source/src/squarestats_server_event.h
@@ -1,4 +1,16 @@
 /* squarestats_server_event.h */
+#ifndef _SQUARESTATS_SERVER_EVENT_H_
+#define _SQUARESTATS_SERVER_EVENT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void on_event_kill(const char* killer, const char* victim, const char* weapon,
                    unsigned int time, int gib, int suicide);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* _SQUARESTATS_SERVER_EVENT_H_ */
