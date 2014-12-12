d1e20d43e73cca0344c2460b7b32b27b5d92787b
Rikki Guy <git@rikkiguy.me.uk>
2014-12-12 17:38:20 +0000

    Add more test cases for event_kill_json(...), plus JSON testing utility functions.

--- source/src/squarestats_event_json_test.c
+++ source/src/squarestats_event_json_test.c
@@ -2,7 +2,46 @@
 
 #include <stdlib.h>
 #include <check.h>
+#include "cJSON.h"
 #include "squarestats_event_json.h"
+
+/*******************************************************************************
+ * JSON-checking utility functions
+ ******************************************************************************/
+
+void ck_json_item_string(cJSON* obj, const char* item, const char* str)
+{
+        cJSON* jitem = cJSON_GetObjectItem(obj, item);
+        ck_assert(jitem != NULL);
+        ck_assert(jitem->type == cJSON_String);
+        ck_assert_str_eq(str, jitem->valuestring);
+}
+
+void ck_json_item_double(cJSON* obj, const char* item, double num)
+{
+        cJSON* jitem = cJSON_GetObjectItem(obj, item);
+        ck_assert(jitem != NULL);
+        ck_assert(jitem->type == cJSON_Number);
+        ck_assert(num == jitem->valuedouble);
+}
+
+void ck_json_item_true(cJSON* obj, const char* item)
+{
+        cJSON* jitem = cJSON_GetObjectItem(obj, item);
+        ck_assert(jitem != NULL);
+        ck_assert(jitem->type == cJSON_True);
+}
+
+void ck_json_item_false(cJSON* obj, const char* item)
+{
+        cJSON* jitem = cJSON_GetObjectItem(obj, item);
+        ck_assert(jitem != NULL);
+        ck_assert(jitem->type == cJSON_False);
+}
+
+/*******************************************************************************
+ * Tests
+ ******************************************************************************/
 
 START_TEST(test_frag)
 {
@@ -12,27 +51,66 @@ START_TEST(test_frag)
                 0, 0
         );
         
-        ck_assert_str_eq(json, "{ \"victim\": \"k1ll3r\" }");
+        cJSON* obj = cJSON_Parse(json);
         
+        ck_json_item_string(obj, "killer", "k1ll3r");
+        ck_json_item_string(obj, "victim", "v!icTim");
+        ck_json_item_string(obj, "weapon", "shotgun");
+        ck_json_item_double(obj, "time", 12.345);
+        ck_json_item_false(obj, "gib");
+        ck_json_item_false(obj, "suicide");
+        
+        /* We do not test that killer != victim when not suicide, as it is
+         * possible that two different people with the same name killed one
+         * another, so this situation is valid. */
+
+        cJSON_Delete(obj);
         free((void*) json);
 }
 END_TEST
 
-START_TEST(test_fail)
+START_TEST(test_gib)
 {
-        ck_assert(0);
+        const char* json = event_kill_json(
+                "k1ll3r", "v!icTim",
+                "shotgun", 12345,
+                1, 0
+        );
+        
+        cJSON* obj = cJSON_Parse(json);
+        
+        ck_json_item_true(obj, "gib");
+        
+        cJSON_Delete(obj);
+        free((void*) json);
+}
+END_TEST
+
+START_TEST(test_suicide)
+{
+        const char* json = event_kill_json(
+                "idiot", "idiot",
+                "grenade", 12345,
+                0, 1
+        );
+        
+        cJSON* obj = cJSON_Parse(json);
+        
+        ck_json_item_true(obj, "suicide");
+        
+        cJSON_Delete(obj);
+        free((void*) json);
 }
 END_TEST
 
 Suite* event_json_suite(void)
 {
-        Suite* suite = suite_create("event_json");
-        
         TCase* tcase_core = tcase_create("core");
-        
         tcase_add_test(tcase_core, test_frag);
-        tcase_add_test(tcase_core, test_fail);
+        tcase_add_test(tcase_core, test_gib);
+        tcase_add_test(tcase_core, test_suicide);
         
+        Suite* suite = suite_create("event_json");
         suite_add_tcase(suite, tcase_core);
         
         return suite;
