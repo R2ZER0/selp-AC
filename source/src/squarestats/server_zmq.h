0539ea0199760a40a82048dbe627bb2a2defbf22
Rikki Guy <git@rikkiguy.me.uk>
2014-12-16 21:37:34 +0000

    Added header server_zmq.h

---
+++ source/src/squarestats/server_zmq.h
@@ -0,0 +1,15 @@
+/* server_zmq.h */
+#ifndef _SQUARESTATS_SERVER_ZMQ_H_
+#define _SQUARESTATS_SERVER_ZMQ_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+void send_message(const char* message);
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SQUARESTATS_SERVER_ZMQ_H_ */
