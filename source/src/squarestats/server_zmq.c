aa338c6b115e3ca4f1dc455ea8738cbd5a590900
Rikki Guy <git@rikkiguy.me.uk>
2014-12-16 21:34:04 +0000

    Finish simple implementation of zmq message sending

--- source/src/squarestats/server_zmq.c
+++ source/src/squarestats/server_zmq.c
@@ -1,17 +1,39 @@
 /* server_zmq.c */
 
 #include <stdlib.h>
+#include <string.h>
 #include <zmq.h>
 
 void* context = NULL;
 void* pub = NULL;
 
+void init_zmq(void);
+void deinit_zmq(void);
+
+/* Create our socket */
 void init_zmq(void)
 {
         context = zmq_context_new();
+        pub = zmq_socket(context, ZMQ_PUB);
+        atexit(deinit_zmq);
 }
 
+/* Clean up */
+void deinit_zmq(void) {
+        zmq_close(pub);
+        pub = NULL;
+        
+        zmq_ctx_destroy(context);
+        context = NULL;
+}
+
+/* Send the given string as a simple one-part message */
 void send_message(const char* message)
 {
-    
+        if(!pub) { init_zmq(); }
+        
+        /* Send the message with DONTWAIT, we cannot afford to block the server,
+         * so if there is not enough space in the (rather large) zmq socket
+         * buffer, this is not NASA, missing one bit of data won't hurt. */
+        zmq_send(pub, message, strlen(message), ZMQ_DONTWAIT);
 }
